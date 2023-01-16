7abb0a7eecceaf6fb48aa7d994264e23d5664fa0
EmanuelBenitezBaez <95762072+EmanuelBenitezBaez@users.noreply.github.com>
2023-01-16 16:34:07 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,14 +9,14 @@ Ejercicio 2
 Escribe un programa que tome la hora y la escriba por pantalla.
  */
 //Nombre y Hora
-Console.WriteLine("Ingresa tu nombre:");
+Console.WriteLine(" Ingresa tu nombre:");
 string name = Console.ReadLine();
 
-Console.WriteLine("Ingrese la hora:");
+Console.WriteLine(" Ingrese la hora:");
 string time = Console.ReadLine();
 
 
 
-Console.WriteLine("Hola:"+ name );
-Console.WriteLine("Son las:" + time  +  "horas.");
+Console.WriteLine(" Hola:"+ name );
+Console.WriteLine(" Son las:" + time  +  " horas.");
 
