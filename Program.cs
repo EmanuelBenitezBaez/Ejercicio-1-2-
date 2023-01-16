1ab0963beef57b743162f9d1d89c7ca2511dfa74
EmanuelBenitezBaez <95762072+EmanuelBenitezBaez@users.noreply.github.com>
2023-01-16 15:56:08 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+/*
+ Ejercicio 1
+
+Escribe un programa que reciba tu nombre y lo escriba por consola.
+
+Ejercicio 2
+
+Escribe un programa que tome la hora y la escriba por pantalla.
+ */
+//Nombre y Hora
+Console.WriteLine("Ingresa tu nombre:");
+string name = Console.ReadLine();
+
+Console.WriteLine("Ingrese la hora:");
+string time = Console.ReadLine();
+
+
+
+Console.WriteLine("Hola"+ name );
+Console.WriteLine("Son las:" + time  +  "horas.");
+
